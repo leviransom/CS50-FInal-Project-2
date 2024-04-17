d5788c252b88da5b09953f6a36222b2ccc1125c1
leviransom <leviransom90@gmail.com>
2024-04-17 17:01:45 -0400

    5:01pm

---
+++ project.py
@@ -0,0 +1,8 @@
+i = True
+while i:
+    user_input = input("Insert your question here or 'Quit' to quit: ")
+    if user_input.lower() == "quit":
+        i = False        
+    if i:        
+        '''Paste jp code here replace test prompt area with user_input variable'''
+print("Program Terminated")
