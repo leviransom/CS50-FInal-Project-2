e0512780f0ecdc83982378c7cca305308675d869
jul1anpa <paynejulian1998@gmail.com>
2024-04-17 16:00:43 -0400

    updated code @ 16:00 -jp

--- project.py
+++ project.py
@@ -1,2 +1,18 @@
+import os
+from openai import OpenAI
 
-print("H")
+client = OpenAI(
+    api_key = "sk-proj-5AnjLnqzLJeOnQuL4yfXT3BlbkFJTq3DUIwpSIVt6kKbqK0z"
+)
+
+chat_completion = client.chat.completions.create(
+    messages = [
+        {
+            "role": "user",
+            "content": "ßWhat team won nba finals 2012",
+        }
+    ],
+    model = "gpt-3.5-turbo"
+)
+
+print(chat_completion.choices[0].message.content)
